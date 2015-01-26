3b6fcc899068b4a387890b5505640b101ee2aa1f
Readman Chiu <rchiu@bcgsc.ca>
2015-01-26 15:29:05 -0800

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import os
 from setuptools import setup, find_packages
 execfile(os.path.dirname(os.path.realpath(__file__)) + "/version.py")
-setup(name='pavfinder',
-      version=__version__,
-      author='Readman Chiu',
-      packages=find_packages()
-      )
+setup(
+    name='pavfinder',
+    version=__version__,
+    description='Post Assembly Variant Finder',
+    long_description='Identifies genomic structural variants or transcriptomic splice variants given the alignments of assembly contigs',
+    url='https://github.com/bcgsc/pavfinder.git',
+    author='Readman Chiu',
+    author_email='rchiu@bcgsc.ca',
+    license='BCCA',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        ],
+    packages=find_packages(),
+    install_requires = [
+        'pysam>=0.7.7,<0.8.1',
+        'pybedtools>=0.6.2',
+        'intspan>=0.701',
+        ],
+)
